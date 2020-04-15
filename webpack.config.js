9b56cd03c9069b0cf50078b1e1554cda5287f6d8
Michele Nasti <michele.nasti@gmail.com>
2020-04-15 15:05:10 +0200

    the library is now umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 module.exports = {
     "mode": "development",
     "devtool": "source-map",
     "entry": "./src/index.js",
     "output": {
-        library: 'SoundManager'
+        library: 'SoundManager',
+        libraryTarget: 'umd',
+        filename: 'soundmanager.js',
+        path: path.resolve(__dirname, 'dist'),
     }
 }
